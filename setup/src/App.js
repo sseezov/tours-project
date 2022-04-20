64c4e2a2c2a5a88f827c2529f704642579300d0d
sseezov <sseezov@mail.ru>
2022-04-20 22:39:33 +0700

    initial

---
+++ setup/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
+}
+
+export default App;
