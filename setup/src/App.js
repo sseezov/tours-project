e1b4ca9d7f6c65da3324a9f7f7233677a7469778
sseezov <sseezov@mail.ru>
2022-04-21 15:52:20 +0700

    tours done

--- setup/src/App.js
+++ setup/src/App.js
@@ -6,6 +6,32 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => {
+      return tour.id !== id;
+    });
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   if (loading) {
     return (
       <main>
@@ -13,9 +39,26 @@ function App() {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button
+            className="btn"
+            onClick={() => {
+              fetchTours();
+            }}
+          >
+            check new tours
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
-      <Tours />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
